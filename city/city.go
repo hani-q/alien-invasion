da93c9c49e06fef38fbd78b302bd3f9e5ab11e60
Hani Q <hanan.ilyas@gmail.com>
2021-11-12 19:20:34 +0500

    Basic City and Road structs
    Signed-off-by: Hani Q <hanan.ilyas@gmail.com>

--- city/city.go
+++ city/city.go
@@ -1,12 +1,48 @@
 package city
 
-import "fmt"
+import (
+	"fmt"
+)
 
-type City struct {
-	Name       string   //Name of city
-	Directions []string // Bridges leading out from city
+type Direction int
+
+const (
+	North Direction = iota
+	South
+	East
+	West
+)
+
+func (d Direction) String() string {
+	switch d {
+	case North:
+		return "North"
+	case South:
+		return "South"
+	case East:
+		return "East"
+	case West:
+		return "West"
+
+	default:
+		return "--"
+	}
 }
 
-func (c *City) String() string {
-	return fmt.Sprintf("%v\n", (&c).Name)
+type City struct {
+	Name                     string
+	North, South, East, West Road
+}
+
+type Road struct {
+	DirName  Direction
+	DestCity *City
+}
+
+func (r Road) String() string {
+	return fmt.Sprintf("%v =>%v", r.DirName, r.DestCity.Name)
+}
+
+func (c City) String() string {
+	return fmt.Sprintf("======%v======\n\t%v\n%v\t%v\n\t%v\n=============", c.Name, c.North, c.East, c.West, c.South)
 }
--- main.go
+++ main.go
@@ -7,7 +7,17 @@ import (
 
 func main() {
 
-	c := city.City{Name: "Hani"}
+	c1 := city.City{Name: "Islamabad"}
+	c2 := city.City{Name: "Lahore"}
+	c3 := city.City{Name: "Faisalabad"}
+	c4 := city.City{Name: "Karachi"}
+	c5 := city.City{Name: "Quetta"}
 
-	fmt.Println(c)
+	c1.North = city.Road{DirName: city.North, DestCity: &c2}
+	c1.South = city.Road{DirName: city.South, DestCity: &c3}
+	c1.East = city.Road{DirName: city.East, DestCity: &c4}
+	c1.West = city.Road{DirName: city.West, DestCity: &c5}
+
+	fmt.Println(c1)
+
 }
